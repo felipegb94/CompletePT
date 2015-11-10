5c18206cb7431d4ce36c6f28dce82736158fa2f1
Felipe Gutierrez <fgutierrez3@wisc.edu>
2015-11-10 11:52:45 -0600

    Changed location of certain files. Still need to make the code much more user friendly

--- demo_TwoSamplePermTestingCPU.cpp
+++ demo_TwoSamplePermTestingCPU.cpp
@@ -1,5 +1,7 @@
+#include <vector>
 
 #define ARMA_64BIT_WORD
+#include <string>
 #include "armadillo"
 #include "PermTestingCPU.h"
 #include "Utils/TimerCPU.h"
@@ -8,27 +10,45 @@
 
 int main()
 {
-    std::string dataArmaPath = "/home/felipe/PermTest/data/raw_adrc/adrc_raw.arma";
-
-    int nGroup1 = 25;
+    std::string dataArmaPath = "/home/felipe/PermTest/data/ADRC/ADRC_100_50_50.arma";
+    int numRuns = 8;
+    int nPermutations[] = {2500, 5000, 10000, 20000, 40000, 80000, 160000, 320000};
+    int nGroup1 = 50;
     int nGroup2;
     int N;
     int V;
-    int nPermutations = 10000;
     int i;
     double maxMemory = 5e8; // Max number of bytes to use
 
     arma::mat data;
+    arma::mat timings = arma::zeros(2, numRuns);
     data.load(dataArmaPath);
 
     CpuTimer t;
-    t.Start();
-    t.Start_cputimer();
+    std::string prefix = "timing_CompletePT_ADRC";
 
-    TwoSamplePermTestingCPU(data, nPermutations, nGroup1, maxMemory);
-    t.Stop();
-    t.Stop_cputimer();
+    for(int i = 0;i < numRuns;i++)
+    {
+        t.Start();
+        t.Start_cputimer();
+        TwoSamplePermTestingCPU(data, nPermutations[i], nGroup1, maxMemory);
+        t.Stop();
+        t.Stop_cputimer();
+        t.print();
+        timings(0,i) = t.Elapsed();
+        timings(1,i) = t.Elapsed_cputimer();
 
-    t.print();
+        std::stringstream armaFileName;
+        std::stringstream asciiFileName;
+        armaFileName << prefix << "_" << nPermutations[i] << ".arma";
+        asciiFileName << prefix << "_" << nPermutations[i] << ".ascii";  
+        timings.save(armaFileName.str());
+        timings.save(asciiFileName.str(), arma::raw_ascii);
+    }
+
+
+
+
+
     return 0;
 }
